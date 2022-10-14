47b6eb62b7823a743f76db7db6c446a6ff474480
user.email <charlesguevara273@gmail.com>
2022-10-13 20:48:14 -0400

    second commit

--- src/ReviewClasses/School.java
+++ src/ReviewClasses/School.java
@@ -4,5 +4,14 @@ public class School {
     public static void main(String[] args) {
         Teacher t = new Teacher("John", "Smith");
         System.out.println(t.name);
+
+        new Teacher("Jane","Smith","Biology").teach();
+
+        new Teacher("Jal","Smoke",8).print();
+
+        Teacher.school="Syntax";
+
+
+
     }
 }
--- src/ReviewClasses/Teacher.java
+++ src/ReviewClasses/Teacher.java
@@ -9,16 +9,44 @@ public class Teacher {
     static String school;
 
     // this. - used to refer to the current object: instance variables
-                                                  // instance methods
-    Teacher(String name, String lname){
-        this.name=name;
-        this.lname=lname;
+    // instance methods
+    Teacher(String name, String lname) {
+        this.name = name;
+        this.lname = lname;
     }
     //this() - refers to the current object: current class constructor
 
-    Teacher(String name, String lname,String subject){
-
+    Teacher(String name, String lname, String subject) {
+        this(name, lname); //Constructor call must be the first statement in a
+        this.subject = subject;
     }
 
+    Teacher(String name, String lname, int experience) {
+        this(name,lname);
+        this.experience=experience;
+    }
 
+    // static can only work with static
+    static void work(){
+        System.out.println("All teachers work at"+school);
+    }
+    //instance member of the class can work with instance and static members
+     void print() {
+        work();
+        System.out.println("Teacher name is "+name+" "+lname);
+    }
+
+    void teach() {
+        print();
+        System.out.println(name+" teaches "+subject);
+    }
+
+    //method that will calculate bonus based on experience and return it
+        double getBonus() {
+        if(experience<5){
+            return 3;
+        } else{
+            return 3.5;
+        }
+    }
 }
