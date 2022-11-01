bdaa8500a42e3bceeb6ac08247eb31d0f72a4e7d
user.email <charlesguevara273@gmail.com>
2022-10-31 21:52:22 -0400

    new changes

---
+++ src/class25/Hw1.java
@@ -0,0 +1,19 @@
+package class25;
+
+import java.util.ArrayList;
+
+public class Hw1 {
+    public static void main(String[] args) {
+        /*Create an arrayList of words. Remove every word that ends with “e”  */
+
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("Syntax");
+        words.add("Cats");
+        words.add("Remote");
+        words.add("Doge");
+        words.add("Tacos");
+
+
+
+    }
+}
